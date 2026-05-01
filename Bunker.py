6e5a2ff89890e30c57ccb636eeb133f651adf68d
Salvador-Navarro-Code <salvadornavarrodev1990@gmail.com>
2026-04-30 19:51:03 -0600

    Create Bunker.py
    lo que hice aqui fue una anidacion de 16 lineas  de if y else  pidiendo un código/password para que deje entrar al usuario a su cuenta personal y después de tres intentos  se bloquea el acceso a dicha cuenta por motivos de seguridad

---
+++ Bunker.py
@@ -0,0 +1,17 @@
+password = input("cual es la contraseña?.")
+if"0509" in password:
+	print(" por favor ingrese el numero que le llegara via sms.")
+else:
+	print("La contraseña no coincide")
+	second_attempt = input("Le quedan 2 intentos para poner la contraseña")
+	if"0509" in second_attempt:
+		print("Bienvenido")
+	else:
+			print("La contraseña sigue sincoincidir")
+	third_attempt = input("este es su ultimo intento")
+	if"0509" in third_attempt:
+		print("Bienvenido justo en el ultimo intento")
+	else:
+			print("se mando un correo electronico para restablecer tu cuenta")
+			print("por motivos de seguridad tu usuario fue bloqueado por multibles intentos erróneos")
+			#No es mucho pero es trabajo honesto 
